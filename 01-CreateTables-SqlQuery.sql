85aef33cafb4374ab0265b7ed9eedf467afa23d4
NareVellela <nare.vellela@nt.gov.au>
2017-09-10 20:25:01 +0930

    Adding Scipts

---
+++ 01-CreateTables-SqlQuery.sql
@@ -0,0 +1,22 @@
+﻿Create table Category(
+[CategoryId]			[int]	IDENTITY(1,1)	NOT NULL,
+[CategoryName]			[nvarchar](100)			NOT NULL,
+[CategoryDescription]	[nvarchar](max)			NOT NULL,
+
+CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([CategoryId] ASC),
+CONSTRAINT [UK_Category_CategoryName] UNIQUE NONCLUSTERED ([CategoryName] ASC)
+)
+
+GO
+
+Create table Movie(
+[MovieId]			[int]	IDENTITY(1,1)	NOT NULL,
+[MovieName]			[nvarchar](100)			NOT NULL,
+[MovieDescription]	[nvarchar](max)			NOT NULL,
+[CategoryId]		[int]					NOT NULL,
+[IsActive]			[bit]					NOT NULL	DEFAULT ((1)),
+
+CONSTRAINT [PK_Movie] PRIMARY KEY CLUSTERED ([MovieId] ASC),
+CONSTRAINT [UK_Movie_MovieName] UNIQUE NONCLUSTERED ([MovieName] ASC),
+CONSTRAINT [FK_Movie_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] (CategoryId)
+)
